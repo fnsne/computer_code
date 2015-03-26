dcb75c4d7d1fdfea29e5f9fda69b97ddd959ab4b
fnsne <watashiwafnsne@gmail.com>
2015-03-26 21:08:09 +0800

    just a test

---
+++ java/A.java
@@ -0,0 +1,7 @@
+package A;
+public class A{
+	public	void show()
+	{
+			System.out.println("this is in package A.A.java");
+	}
+}
---
+++ java/test.java
@@ -0,0 +1,9 @@
+import A.A;
+public class test{
+	public static void main( String[] args )
+	{
+		System.out.println("this is test.java" );
+		A a = new A();
+		a.show();	
+	}
+}
